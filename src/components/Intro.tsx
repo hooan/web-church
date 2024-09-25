6bb1509001921e87bb76ca527f56814a527357cb
hooan <hooan.cristobal@gmail.com>
2024-09-25 16:32:19 -0600

    changes

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,18 +3,16 @@ import React from 'react';
 const Intro: React.FC = () => {
     return (
         <section id="intro" className="wrapper style1">
-        <div className="title">The Introduction</div>
+        <div className="title">You’re Welcome Here!</div>
         <div className="container">
           <p className="style1">So in case you were wondering what this is all about ...</p>
-          <p className="style2">
-            Escape Velocity is a free responsive<br className="mobile-hide" />
-            site template by <a href="http://html5up.net" className="nobr">HTML5 UP</a>
+          <p className="style2"> Something something
+            <br className="mobile-hide" />
           </p>
-          <p className="style3">It's <strong>responsive</strong>, built on <strong>HTML5</strong> and <strong>CSS3</strong>, and released for
-          free under the <a href="http://html5up.net/license">Creative Commons Attribution 3.0 license</a>, so use it for any of
-          your personal or commercial projects &ndash; just be sure to credit us!</p>
+          <p className="style3"><strong>Oasis de fe</strong> is one church meeting in multiple locations, and we want to help you become the person God made you to be. No matter where you are in your journey, you’re invited to discover your purpose and live it out at 
+          <strong> Oasis de fe</strong>.</p>
           <ul className="actions">
-            <li><a href="#" className="button style3 large">Proceed</a></li>
+            <li><a href="#" className="button style3 large">Find a Location</a></li>
           </ul>
         </div>
       </section>
